451b36939ed7cb91ce86c4d25a8ca29aeefd5e5a
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-06-09 11:09:56 +0200

    Fix routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,8 +8,8 @@ const { schema } = require('../../shemas/contacts');
 const router = express.Router();
 
 router.get('/', ctrl.listContacts);
-router.get('/:contactId', validateBody(schema), ctrl.getContactById);
-router.post('/', ctrl.addContact);
+router.get('/:contactId', ctrl.getContactById);
+router.post('/', validateBody(schema), ctrl.addContact);
 router.delete('/:contactId', ctrl.removeContact);
 router.put('/:contactId', validateBody(schema), ctrl.updateContact);
 
