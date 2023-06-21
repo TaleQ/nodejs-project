54a4e680c7e6026b183f2226748e9fb1b7e8db47
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-06-21 12:02:03 +0200

    Add db-connection using mongoose

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 
-const { validateBody } = require('../../middlewares');
-const { schema } = require('../../shemas/contacts');
+// const { validateBody } = require('../../middlewares');
+// const { schema } = require('../../shemas/contacts');
 
 const router = express.Router();
 
 router.get('/', ctrl.listContacts);
-router.get('/:contactId', ctrl.getContactById);
-router.post('/', validateBody(schema), ctrl.addContact);
-router.delete('/:contactId', ctrl.removeContact);
-router.put('/:contactId', validateBody(schema), ctrl.updateContact);
+// router.get('/:contactId', ctrl.getContactById);
+// router.post('/', validateBody(schema), ctrl.addContact);
+// router.delete('/:contactId', ctrl.removeContact);
+// router.put('/:contactId', validateBody(schema), ctrl.updateContact);
 
 module.exports = router;
