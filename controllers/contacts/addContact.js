e3adf6de57754403a6df1034d674221859b142af
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-06-27 16:18:08 +0200

    Update contacts controllers

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,7 +1,8 @@
 const { contactModel } = require('../../models');
 
 const addContact = async (req, res) => {
-  const result = await contactModel.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await contactModel.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,7 +1,14 @@
 const { contactModel } = require('../../models');
 
-const listContacts = async (_req, res) => {
-  const result = await contactModel.find();
+const listContacts = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await contactModel.find({ owner }, '', {
+    skip: skip,
+    limit: limit,
+  });
   res.status(200).json(result);
 };
 
